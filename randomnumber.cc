7b234af0c7af6d3321609638221c326da7278539
KRISHNA CHANDHOK (JUGAAD) <52276473+krishna619@users.noreply.github.com>
2020-03-30 16:43:39 -0400

    randomnumber.cc

---
+++ randomnumber.cc
@@ -0,0 +1,36 @@
+// GENERATE RANDOM NUMBER WITHOUT USING RAND() FUNCTION.(GNU OCTAVE)//
+1.)OPEN OCTAVE
+2.)TYPE "mkoctfile --link-stand-alone randomnumber.cc -o randomnumber"
+3.) Make sure liboctave pkg is already installed on your linux system.
+
+
+#include <octave/oct.h>
+#include<stdio.h>
+#include<time.h>
+int main()
+{
+int num=5;
+time_t sec;
+char option;
+sec=time(NULL);
+printf("\nDo you want to print random number? {Y|N) ");
+scanf("%c", &option);
+if (option =='y' || option =='Y')
+{
+for(;;)
+{
+sec=sec%3600;
+if(num>=sec)
+{
+printf("%ld\n",sec);
+break;
+}
+sec=sec%num;
+}
+}
+else
+{
+printf("enter valid selection");
+}
+return 0;
+}
