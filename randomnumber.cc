5745e72ba4bd06608960c7404c9196c9959c9303
KRISHNA CHANDHOK <52276473+krishna619@users.noreply.github.com>
2021-04-17 01:15:39 +0530

    Update randomnumber.cc

--- randomnumber.cc
+++ randomnumber.cc
@@ -7,30 +7,25 @@
 #include <octave/oct.h>
 #include<stdio.h>
 #include<time.h>
-int main()
-{
-int num=5;
-time_t sec;
-char option;
-sec=time(NULL);
-printf("\nDo you want to print random number? {Y|N) ");
-scanf("%c", &option);
-if (option =='y' || option =='Y')
-{
-for(;;)
-{
-sec=sec%3600;
-if(num>=sec)
-{
-printf("%ld\n",sec);
-break;
-}
-sec=sec%num;
-}
-}
-else
-{
-printf("enter valid selection");
-}
-return 0;
+
+int main() {
+  int num = 5;
+  time_t sec;
+  char option;
+  sec = time(NULL);
+  printf("\nDo you want to print random number? {Y|N) ");
+  scanf("%c", & option);
+  if (option == 'y' || option == 'Y') {
+    for (;;) {
+      sec = sec % 3600;
+      if (num >= sec) {
+        printf("%ld\n", sec);
+        break;
+      }
+      sec = sec % num;
+    }
+  } else {
+    printf("enter valid selection");
+  }
+  return 0;
 }
